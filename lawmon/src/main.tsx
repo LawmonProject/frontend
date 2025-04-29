86c0c1cbb4bc9f94269d6869d445c7441ffc1d9d
이현빈 <lhb0107@naver.com>
2025-04-29 18:13:47 +0900

    feat : Tanstack devtool

--- lawmon/src/main.tsx
+++ lawmon/src/main.tsx
@@ -1,9 +1,13 @@
-
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { router } from 'app/router';
 import './index.css';
+import { QueryProvider } from 'app/providers/QueryProvider';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 createRoot(document.getElementById('root')!).render(
-  <RouterProvider router={router} />
+  <QueryProvider>
+    <ReactQueryDevtools initialIsOpen={false} />
+    <RouterProvider router={router} />
+  </QueryProvider>,
 );

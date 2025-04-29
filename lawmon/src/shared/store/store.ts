9f3fc844ad64b57f97680881d731f3830df25a29
이현빈 <lhb0107@naver.com>
2025-04-29 18:25:49 +0900

    feat : zustand 상태 저장 기능 구현

--- lawmon/src/shared/store/store.ts
+++ lawmon/src/shared/store/store.ts
@@ -3,9 +3,21 @@ import { create } from 'zustand';
 interface ContractState {
   selectedFile: File | null;
   setSelectedFile: (file: File | null) => void;
+  contractID: number | null;
+  setContractID: (id: number | null) => void;
+  title : string | null;
+  setTitle: (title: string | null) => void;
+  category: string | null;
+  setCategory: (category: string | null) => void;
 }
 
 export const useContractStore = create<ContractState>((set) => ({
+  contractID: null,
+  setContractID: (id) => set({ contractID: id }),
+  title: null,
+  setTitle: (title) => set({ title }),
+  category: null,
+  setCategory: (category) => set({ category }),
   selectedFile: null,
   setSelectedFile: (file) => set({ selectedFile: file }),
 }));
